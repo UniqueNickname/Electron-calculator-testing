af44528e3afadde9841e273d9fab570e4dec1cd8
UniqueNickname <dr.art-47@yandex.ru>
2019-05-16 21:51:33 +0600

    placementNo_Ready

--- js/enterNForm.js
+++ js/enterNForm.js
@@ -1,5 +1,20 @@
 function valid(form) {
-    let n = form.enterN.value;
+    let n   = form.enterN.value;
     let res = Factorial(n);
-    console.log(res);
+    ////console.log(res);
+
+    ////var text = document.getElementById("placementNoRES");
+    ////text.innerHTML = "Two";
+    ////text.childNodes[0].nodeValue=res;
+
+    ////const text = "Число перестановок: ";
+    document.getElementById("placementNoRES") .innerHTML = res ;
+    ////out(res,  "placementNoRES" );
+    ////out(text, "placementNoTEXT");
+    ////document.getElementById("placementNoTEXT").innerHTML = text;
+    return res;
 }
+
+////function out(text, id) {
+////    document.getElementById(id).firstChild.nodeValue = text;
+////}
